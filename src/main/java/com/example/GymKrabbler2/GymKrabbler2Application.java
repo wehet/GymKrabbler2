7b996ea2b4aef3a10a3eeeb0467a2fc7a0172d70
wehet <tim99wehe@gmail.com>
2021-05-22 11:16:18 +0200

    init

---
+++ src/main/java/com/example/GymKrabbler2/GymKrabbler2Application.java
@@ -0,0 +1,13 @@
+package com.example.GymKrabbler2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymKrabbler2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymKrabbler2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GymKrabbler2/GymKrabbler2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymKrabbler2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymKrabbler2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
