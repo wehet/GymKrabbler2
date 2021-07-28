2a22e151c123de27b82b5fbed0dea5fd24043bc0
Zoe-Christ <zoe.christ@mail.de>
2021-07-28 18:35:36 +0200

    aktualisiert

--- src/main/java/com/example/GymKrabbler2/webScraper/exceptions/ScrapeException.java
+++
@@ -1,9 +0,0 @@
-package com.example.GymKrabbler2.webCrawler.exceptions;
-
-public class ScrapeException extends Exception {
-
-	public ScrapeException(String errormessage) {
-		super(errormessage);
-	}
-
-}
